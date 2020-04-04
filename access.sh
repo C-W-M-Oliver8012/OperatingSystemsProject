6d1309a469753edf66f8804a873281764e4bfa06
Caden <cadenmiller65@yahoo.com>
2020-04-04 15:52:24 -0500

    updated the access file

--- access.sh
+++ access.sh
@@ -13,46 +13,66 @@ function print_menu_header
 }
 
 if [[ ( -d "Financial_Files" && -d "General_Files" && -d "Project_Files" ) ]]; then
-	print_menu_header
-	printf "   | ${WHITE}1) General Files                            ${YELLOW}|\n"
-	printf "   | ${WHITE}2) Project Files                            ${YELLOW}|\n"
-	
-	if [ "$1" == "power_user" ]; then
-		printf "   | ${WHITE}3) Financial Files                          ${YELLOW}|\n"
-	fi
-	
-	printf "   ===============================================\n\n"
-	printf "${WHITE}"
-	read -p "   Directory to Open: " input
-	
-	if [ "$input" == 1 ]; then
-		clear
+
+	while [[ ( "$1" == "power_user" && "$input" != 4 ) || ( "$1" == "general_user" && "$input" != 3 ) ]]; do
+		
 		print_menu_header
-		printf "   | ${WHITE}General Files                               ${YELLOW}|\n"
-		printf "   ===============================================${WHITE}\n"
-		for entry in "General_Files"/*; do
-			printf "   $entry\n"
-		done
-	elif [ "$input" == 2 ]; then
-		clear
-		print_menu_header
-		printf "   | ${WHITE}Project Files                               ${YELLOW}|\n"
-		printf "   ===============================================${WHITE}\n"
-		for entry in "Project_Files"/*; do
-			printf "   $entry\n"
-		done
-	elif [[ ( "$input" == 3 && "$1" == "power_user" ) ]]; then
-		clear
-		print_menu_header
-		printf "   | ${WHITE}Financial Files                             ${YELLOW}|\n"
-		printf "   ===============================================${WHITE}\n"
-		for entry in "Financial_Files"/*; do
-			printf "   $entry\n"
-		done
-	fi
-	
-	printf "\n"
-	read -p "   Press enter to return to menu..." input
+		printf "   | ${WHITE}1) General Files                            ${YELLOW}|\n"
+		printf "   | ${WHITE}2) Project Files                            ${YELLOW}|\n"
+		
+		if [ "$1" == "power_user" ]; then
+			printf "   | ${WHITE}3) Financial Files                          ${YELLOW}|\n"
+			printf "   | ${WHITE}4) Return to user menu                      ${YELLOW}|\n"
+		elif [ "$1" == "general_user" ]; then
+			printf "   | ${WHITE}3) Return to user menu                      ${YELLOW}|\n"
+		fi
+		
+		printf "   ===============================================\n\n"
+		printf "${WHITE}"
+		read -p "   Directory to Open: " input
+		
+		if [ "$input" == 1 ]; then
+			clear
+			print_menu_header
+			printf "   | ${WHITE}General Files                               ${YELLOW}|\n"
+			printf "   ===============================================\n"
+			i=1
+			for entry in "General_Files"/*; do
+				printf "   | ${WHITE}%2d) %-39s ${YELLOW}|\n" "$i" "$entry"
+				i=$((i+1))
+			done
+			printf "   ===============================================\n"
+			
+		elif [ "$input" == 2 ]; then
+			clear
+			print_menu_header
+			printf "   | ${WHITE}Project Files                               ${YELLOW}|\n"
+			printf "   ===============================================\n"
+			i=1
+			for entry in "Project_Files"/*; do
+				printf "   | ${WHITE}%2d) %-39s ${YELLOW}|\n" "$i" "$entry"
+			done
+			printf "   ===============================================\n"
+			
+		elif [[ ( "$input" == 3 && "$1" == "power_user" ) ]]; then
+			clear
+			print_menu_header
+			printf "   | ${WHITE}Financial Files                             ${YELLOW}|\n"
+			printf "   ===============================================\n"
+			i=1
+			for entry in "Financial_Files"/*; do
+				printf "   | ${WHITE}%2d) %-39s ${YELLOW}|\n" "$i" "$entry"
+			done
+			printf "   ===============================================\n"
+		fi
+		
+		if [ "$input" != 4 ]; then
+			printf "${WHITE}\n"
+			read -p "   Press enter to return to menu..." input
+		fi
+		
+	done
+
 fi
 
 

3d598481baa3a43741bb4d0ae0f042ff7aa80a64
Caden <cadenmiller65@yahoo.com>
2020-04-10 06:22:38 -0500

    refractered access.sh

--- access.sh
+++ access.sh
@@ -14,155 +14,102 @@ function print_menu_header
 	printf "   ===============================================\n"
 }
 
+function print_directories_and_get_input_option
+{
+	if [ "$first_menu" = 1 ]; then
+		print_menu_header "$1" "$2"
+		printf "   |  %bDirectory Options                          %b|\n" "$WHITE" "$YELLOW"
+		printf "   |                                             |\n"
+		printf "   |  %b1) General Files                           %b|\n" "$WHITE" "$YELLOW"
+		printf "   |  %b2) Project Files                           %b|\n" "$WHITE" "$YELLOW"
+
+		if [ "$2" = "power_user" ]; then
+			printf "   |  %b3) Financial Files                         %b|\n" "$WHITE" "$YELLOW"
+			printf "   |  %b4) Return to user menu                     %b|\n" "$WHITE" "$YELLOW"
+		elif [ "$2" = "general_user" ]; then
+			printf "   |  %b3) Return to user menu                     %b|\n" "$WHITE" "$YELLOW"
+		fi
+
+		printf "   ===============================================\n\n"
+		printf "%b   Option: " "$WHITE"
+		read -r directory_option
+
+		if [ "$2" = "power_user" ] && [ "$directory_option" == 4 ]; then
+			loop_option=1
+		elif [ "$2" = "general_user" ] && [ "$directory_option" == 3 ]; then
+			loop_option=1
+		fi
+	fi
+}
+
+function display_and_get_file_to_open
+{
+	clear
+	print_menu_header "$1" "$2"
+	printf "   |  %b%-20s                       %b|\n" "$WHITE" "$3" "$YELLOW"
+	printf "   |                                             |\n"
+	i=1
+	for entry in "$3"/*; do
+		IFS='/' read -r directory file <<< "$entry"
+		printf "   | %b%2d) %-39s %b|\n" "$WHITE" "$i" "$file" "$YELLOW"
+		i=$((i+1))
+	done
+	printf "   | %b%2d) Exit                                    %b|\n" "$WHITE" "$i" "$YELLOW"
+	printf "   ===============================================\n\n"
+	printf "%b   Option: " "$WHITE"
+	read -r file_option
+}
+
+function display_file_contents
+{
+	i=1
+	for entry in "$1"/*; do
+		echo "$entry"
+		if [ "$file_option" = "$i" ]; then
+			file_to_open="$entry"
+		fi
+		i=$((i+1))
+	done
+	clear
+	if (( "$file_option" < "$i" )) && (( "$file_option" > 0 )); then
+		printf "%b" "$GREEN"
+		printf "\n   %s\n\n" "$file_to_open"
+		y=1
+		while read -r line; do
+			printf "%b%4d%b|%b %s\n" "$GREEN" "$y" "$YELLOW" "$WHITE" "$line"
+			y=$((y+1))
+		done < "$file_to_open"
+		read -r exit
+	fi
+	if [ "$file_option" = "$i" ]; then
+		first_menu=1
+		directory_option=0
+	else
+		first_menu=0
+		directory_option="$2"
+	fi
+}
+
 printf "%b" "$BACKGROUND"
 
 first_menu=1
-input=0
+directory_option=0
+loop_option=0
+file_option=0
 
 if [ -d "Financial_Files" ] && [ -d "General_Files" ] && [ -d "Project_Files" ]; then
-	while [ "$2" = "power_user" ] && [ "$input" != 4 ] || [ "$2" = "general_user" ] && [ "$input" != 3 ]; do
-		if [ "$first_menu" = 1 ]; then
-			print_menu_header "$1" "$2"
-			printf "   |  %bDirectory Options                          %b|\n" "$WHITE" "$YELLOW"
-			printf "   |                                             |\n"
-			printf "   |  %b1) General Files                           %b|\n" "$WHITE" "$YELLOW"
-			printf "   |  %b2) Project Files                           %b|\n" "$WHITE" "$YELLOW"
+	while [ "$loop_option" = 0 ]; do
+		print_directories_and_get_input_option "$1" "$2"
 
-			if [ "$2" = "power_user" ]; then
-				printf "   |  %b3) Financial Files                         %b|\n" "$WHITE" "$YELLOW"
-				printf "   |  %b4) Return to user menu                     %b|\n" "$WHITE" "$YELLOW"
-			elif [ "$2" = "general_user" ]; then
-				printf "   |  %b3) Return to user menu                     %b|\n" "$WHITE" "$YELLOW"
-			fi
-
-			printf "   ===============================================\n\n"
-			printf "%b   Option: " "$WHITE"
-			read -r input
-		fi
-
-		if [ "$input" = 1 ]; then
-			clear
-			print_menu_header "$1" "$2"
-			printf "   |  %bGeneral Files                              %b|\n" "$WHITE" "$YELLOW"
-			printf "   |                                             |\n"
-			i=1
-			for entry in "General_Files"/*; do
-				IFS='/' read -r directory file <<< "$entry"
-				printf "   | %b%2d) %-39s %b|\n" "$WHITE" "$i" "$file" "$YELLOW"
-				i=$((i+1))
-			done
-			printf "   | %b%2d) Exit                                    %b|\n" "$WHITE" "$i" "$YELLOW"
-			printf "   ===============================================\n\n"
-			printf "%b   Option: " "$WHITE"
-			read -r input
-			i=1
-			for entry in "General_Files"/*; do
-				echo "$entry"
-				if [ "$input" = "$i" ]; then
-					file_to_open="$entry"
-				fi
-				i=$((i+1))
-			done
-			clear
-			if (( "$input" < "$i" )) && (( "$input" > 0 )); then
-				printf "%b" "$GREEN"
-				printf "\n   %s\n\n" "$file_to_open"
-				y=1
-				while read -r line; do
-					printf "%b%4d%b|%b %s\n" "$GREEN" "$y" "$YELLOW" "$WHITE" "$line"
-					y=$((y+1))
-				done < "$file_to_open"
-				read -r exit
-			fi
-			if [ "$input" = "$i" ]; then
-				first_menu=1
-				input=0
-			else
-				first_menu=0
-				input=1
-			fi
-		elif [ "$input" = 2 ]; then
-			clear
-			print_menu_header "$1" "$2"
-			printf "   |  %bProject Files                              %b|\n" "$WHITE" "$YELLOW"
-			printf "   |                                             |\n"
-			i=1
-			for entry in "Project_Files"/*; do
-				IFS='/' read -r directory file <<< "$entry"
-				printf "   | %b%2d) %-39s %b|\n" "$WHITE" "$i" "$file" "$YELLOW"
-				i=$((i+1))
-			done
-			printf "   | %b%2d) Exit                                    %b|\n" "$WHITE" "$i" "$YELLOW"
-			printf "   ===============================================\n\n"
-			printf "%b   Option: " "$WHITE"
-			read -r input
-			i=1
-			for entry in "Project_Files"/*; do
-				echo "$entry"
-				if [ "$input" = "$i" ]; then
-					file_to_open="$entry"
-				fi
-				i=$((i+1))
-			done
-			clear
-			if (( "$input" < "$i" )) && (( "$input" > 0 )); then
-				printf "%b" "$GREEN"
-				printf "\n   %s\n\n" "$file_to_open"
-				y=1
-				while read -r line; do
-					printf "%b%4d%b|%b %s\n" "$GREEN" "$y" "$YELLOW" "$WHITE" "$line"
-					y=$((y+1))
-				done < "$file_to_open"
-				read -r exit
-			fi
-			if [ "$input" = "$i" ]; then
-				first_menu=1
-				input=0
-			else
-				first_menu=0
-				input=1
-			fi
-		elif [ "$input" = 3 ] && [ "$2" = "power_user" ]; then
-			clear
-			print_menu_header "$1" "$2"
-			printf "   |  %bFinancial Files                            %b|\n" "$WHITE" "$YELLOW"
-			printf "   |                                             |\n"
-			i=1
-			for entry in "Financial_Files"/*; do
-				IFS='/' read -r directory file <<< "$entry"
-				printf "   | %b%2d) %-39s %b|\n" "$WHITE" "$i" "$file" "$YELLOW"
-				i=$((i+1))
-			done
-			printf "   | %b%2d) Exit                                    %b|\n" "$WHITE" "$i" "$YELLOW"
-			printf "   ===============================================\n\n"
-			printf "%b   Option: " "$WHITE"
-			read -r input
-			i=1
-			for entry in "Financial_Files"/*; do
-				echo "$file_to_open"
-				if [ "$input" = "$i" ]; then
-					file_to_open="$entry"
-				fi
-				i=$((i+1))
-			done
-			clear
-			if (( "$input" < "$i" )) && (( "$input" > 0 )); then
-				printf "%b" "$GREEN"
-				printf "\n   %s\n\n" "$file_to_open"
-				y=1
-				while read -r line; do
-					printf "%b%4d%b|%b %s\n" "$GREEN" "$y" "$YELLOW" "$WHITE" "$line"
-					y=$((y+1))
-				done < "$file_to_open"
-				read -r exit
-			fi
-			if [ "$input" = "$i" ]; then
-				first_menu=1
-				input=0
-			else
-				first_menu=0
-				input=1
-			fi
+		if [ "$directory_option" = 1 ]; then
+			display_and_get_file_to_open "$1" "$2" "General_Files"
+			display_file_contents "General_Files" "1"
+		elif [ "$directory_option" = 2 ]; then
+			display_and_get_file_to_open "$1" "$2" "Project_Files"
+			display_file_contents "Project_Files" "2"
+		elif [ "$directory_option" = 3 ] && [ "$2" = "power_user" ]; then
+			display_and_get_file_to_open "$1" "$2" "Financial_Files"
+			display_file_contents "Financial_Files" "3"
 		fi
 	done
 fi
